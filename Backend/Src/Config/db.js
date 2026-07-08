888d1c4d8607d61082615c308b2899cfb5420e02
Tshiva05 <webshiva23@gmail.com>
2026-07-08 10:49:23 +0530

    Add database connection setup with mongoose

---
+++ Backend/Src/Config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const env = require("./env");
+
+async function connectDB() {
+  mongoose.set("strictQuery", true);
+  try {
+    await mongoose.connect(env.mongoUri);
+    // eslint-disable-next-line no-console
+    console.log("[db] Connected to MongoDB Atlas");
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error("[db] Connection error:", err.message);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
+  
